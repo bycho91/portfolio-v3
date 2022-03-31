f4b6dbd636b0285f5009509ac7db0222b33d26d3
benjamin <bycho1991@gmail.com>
2022-03-31 14:45:11 -0500

    added the pern todo blog post

--- static/blogs/blogsData.js
+++ static/blogs/blogsData.js
@@ -1,11 +1,20 @@
 export const blogsData = [
   {
     title: 'Budgety Journey',
-    date: 'Feb. 25, 2022',
+    date: 'March 25, 2022',
     description:
       'Just a few thoughts on my experience working with a team for the first time on my biggest project to date: Budgety.',
     imgUrl: '/images/budgety_icon.png',
     source: 'https://dev.to/bycho91/budgety-journey-118',
     id: 0,
+  },
+  {
+    title: 'PERN.TODO - Full Stack',
+    date: 'March 30, 2022',
+    description:
+      'An update on my first full-stack app from scratch using PERN. Deployed to Heroku.',
+    imgUrl: '/images/pern_blog.png',
+    source: 'https://dev.to/bycho91/perntodo-2blc',
+    id: 1,
   },
 ];
